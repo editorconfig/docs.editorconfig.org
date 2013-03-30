2866e1e86161a4aec5484e0613b4284306769ff5
Hong Xu <hong@topbug.net>
2013-03-29 21:15:54 -0700

    {latest,development} ---> master

--- en/latest/search/pages_65.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['editorconfig_20command',['EditorConfig Command',['../editorconfig.html',1,'']]],
-  ['editorconfig_20file_20format',['EditorConfig File Format',['../editorconfig-format.html',1,'']]],
-  ['editorconfig_20core_20documentation',['EditorConfig Core Documentation',['../index.html',1,'']]]
-];
